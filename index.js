69b116a63f5e6bbd773b63da7f2ea69f8a0365e5
Sanu <sanu@stream.space>
2021-06-11 18:31:34 +0530

    concept

---
+++ index.js
@@ -0,0 +1,62 @@
+//binding
+const name = {
+  firstName: "Sanu",
+  lastName: "Dutta",
+};
+
+function fullName(town, state) {
+  console.log(this.firstName, " ", this.lastName, " ", town, " ", state);
+}
+
+Function.prototype.myBind = function (...args) {
+  const obj = this;
+  const params = args.slice(1);
+  return function (...args2) {
+    obj.apply(args[0], [...params, ...args2]);
+  };
+};
+
+const fullName2 = fullName.myBind(name, "Dimapur");
+fullName2("Nagaland");
+
+//debounce
+const debounce = function (fn, delay) {
+  let timeout;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn(context, args), delay);
+  };
+};
+
+const handleClick = debounce(function () {
+  console.log(this);
+  console.log("clicked");
+}, 1000);
+
+const throttle = function (fn, delay) {
+  let prev = 0;
+  return function (...args) {
+    const now = new Date().getTime();
+    const context = this;
+    if (now - prev >= delay) {
+      prev = now;
+      return fn(this,...args);
+    }
+  };
+};
+
+const throttlingClick = throttle(function () {
+  console.log(this);
+  console.log("clicked");
+}, 5000);
+
+document
+  .getElementById("debounce_button")
+  .addEventListener("click", handleClick);
+
+//throttling
+document
+  .getElementById("throttle_button")
+  .addEventListener("click", throttlingClick);
