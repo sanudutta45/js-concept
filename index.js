6f8b2437769b7ffc00f6d61f2d276fa3f51fbc90
Sanu <sanu@stream.space>
2021-06-12 10:30:59 +0530

    recursive thinking

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const throttle = function (fn, delay) {
     const context = this;
     if (now - prev >= delay) {
       prev = now;
-      return fn(this,...args);
+      return fn(this, ...args);
     }
   };
 };
@@ -60,3 +60,63 @@ document
 document
   .getElementById("throttle_button")
   .addEventListener("click", throttlingClick);
+
+
+// recursive thinking
+
+function sum(a) {
+  return function (b) {
+    if (b) return sum(a + b);
+    return a;
+  };
+}
+
+const sumOfNo = sum(1)(2)(5)();
+
+console.log(sumOfNo);
+
+const user = {
+  name: "Sanu",
+  address: {
+    ward: "05",
+    pinCode: "797112",
+    colony: "Bank Colony",
+    district: "Dimapur",
+    postOffice: "Dimapur SO",
+    state: "Nagaland",
+    Country: "India",
+  },
+  idProof: {
+    adhaar: "503445565657",
+    fingerPrint: "gergdf454fggf",
+    DOB: "28/02/1994",
+  },
+  education: {
+    hsc: {
+      school: "Assembly of god hr. sec. school",
+      address: {
+        state: "nagaland",
+        district: "dimapur",
+        pin: "797112",
+      },
+      percentage: "80",
+    },
+  },
+};
+
+const finalObj = {};
+
+function ObjCreator(obj,parent){
+    for(x in obj){
+        if(obj[x].constructor === Object){
+            ObjCreator(obj[x],`${parent}_${x}`)
+        }
+        else{
+            finalObj[`${parent}_${x}`] = obj[x];
+        }
+    }
+}
+
+ObjCreator(user,"user");
+
+console.log(finalObj);
